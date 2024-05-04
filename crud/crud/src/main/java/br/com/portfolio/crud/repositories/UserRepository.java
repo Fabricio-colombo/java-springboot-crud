6f5414369503265e1bac645aff2dc51b91524aca
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-05-04 10:07:09 -0300

    portfolio finish

--- crud/crud/src/main/java/br/com/portfolio/crud/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.portfolio.crud.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.portfolio.crud.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
